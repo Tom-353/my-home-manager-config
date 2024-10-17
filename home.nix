146f603130e4ed6a154ffde84376744f2d7143a3
Tom Rehak <tomrehak353@gmail.com>
2024-10-17 09:18:40 +0200

    fix gtk error in wofi
    (cherry picked from commit f5beca4654067ef65fd86c326c39492ee1ea0586)

--- home.nix
+++ home.nix
@@ -37,9 +37,9 @@ in
       package = pkgs.papirus-icon-theme;
     };
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      #Settings = ''
+      #  gtk-application-prefer-dark-theme=1
+      #'';
     };
     gtk4.extraConfig = {
       Settings = ''
